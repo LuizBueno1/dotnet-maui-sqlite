a4a6e81140be07adbd23d77f8f8714ec8cb7faa6
Luiz Henrique Bueno <luizbueno.dev@gmail.com>
2025-08-02 15:02:28 -0300

    Add model product

---
+++ MyPurchases/Models/Product.cs
@@ -0,0 +1,13 @@
+﻿using SQLite;
+
+namespace MyPurchases.Models
+{
+    public class Product
+    {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+        public string Description { get; set; }
+        public double Amount { get; set; }
+        public double Price { get; set; }
+    }
+}
