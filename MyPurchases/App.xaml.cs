efe18eaec2fe69fbef861112603a7b066d1dd246
Luiz Henrique Bueno <luizbueno.dev@gmail.com>
2025-08-16 12:54:16 -0300

    Add Sqlite path connection

--- MyPurchases/App.xaml.cs
+++ MyPurchases/App.xaml.cs
@@ -1,7 +1,30 @@
-﻿namespace MyPurchases
+﻿using MyPurchases.Helpers;
+
+namespace MyPurchases
 {
     public partial class App : Application
     {
+        static SQLiteDatabaseHelper _db;
+
+        public static SQLiteDatabaseHelper Db
+        {
+            get
+            {
+                if(_db == null)
+                {
+                    string path = Path.Combine(
+                        Environment.GetFolderPath(
+                            Environment.SpecialFolder.LocalApplicationData),
+                        "database_sqlite_purchases.db3"
+                        );
+
+                    _db = new SQLiteDatabaseHelper(".... db3");
+                }
+
+                return _db;
+            }
+        }
+
         public App()
         {
             InitializeComponent();
