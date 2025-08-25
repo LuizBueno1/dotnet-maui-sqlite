3828c4258752b1a2efaf4df6cfc9c500d1f41621
Luiz Henrique Bueno <luizbueno.dev@gmail.com>
2025-08-25 20:19:43 -0300

    Add total attribute

--- MyPurchases/Models/Product.cs
+++ MyPurchases/Models/Product.cs
@@ -9,5 +9,6 @@ namespace MyPurchases.Models
         public string Description { get; set; }
         public double Amount { get; set; }
         public double Price { get; set; }
+        public double Total { get => Amount * Price;}
     }
 }
