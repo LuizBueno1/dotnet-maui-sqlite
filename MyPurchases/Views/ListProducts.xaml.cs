760cd313caa96c65d3be077b43556d4f2d61c5fe
Luiz Henrique Bueno <luizbueno.dev@gmail.com>
2025-08-25 20:21:24 -0300

    Add search and sum features

--- MyPurchases/Views/ListProducts.xaml.cs
+++ MyPurchases/Views/ListProducts.xaml.cs
@@ -1,10 +1,24 @@
+using System.Collections.ObjectModel;
+using MyPurchases.Models;
+
 namespace MyPurchases.Views;
 
 public partial class ListProducts : ContentPage
 {
+	ObservableCollection<Product> list = new ObservableCollection<Product>();
+
 	public ListProducts()
 	{
 		InitializeComponent();
+
+		product_list.ItemsSource = list;
+	}
+
+	protected async override void OnAppearing()
+	{
+		List<Product> l = await App.Db.GetAll();
+
+		l.ForEach(i => list.Add(i));
 	}
 
     private void ToolbarItem_Clicked(object sender, EventArgs e)
@@ -16,6 +30,31 @@ public partial class ListProducts : ContentPage
 		{
 			DisplayAlert("Ops", ex.Message, "OK");
 		}
+
+    }
+
+    private async void txt_search_TextChanged(object sender, TextChangedEventArgs e)
+    {
+        string query = e.NewTextValue;
+
+        list.Clear();
+
+        List<Product> l = await App.Db.Search(query);
+
+        l.ForEach(i => list.Add(i));
+    }
+
+    private void ToolbarItem_Clicked_1(object sender, EventArgs e)
+    {
+		double sum = list.Sum(i => i.Total);
+
+		string msg = $"The total is {sum:C}";
+
+		DisplayAlert("Total products", msg, "Ok");
+    }
+
+    private void MenuItem_Clicked(object sender, EventArgs e)
+    {
 
     }
 }
