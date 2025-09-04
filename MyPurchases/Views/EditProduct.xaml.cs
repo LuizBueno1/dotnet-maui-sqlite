6fc058503ec34a531a0945001223ed46e9d1cdae
Luiz Henrique Bueno <luizbueno.dev@gmail.com>
2025-09-03 22:26:02 -0300

    Add update

--- MyPurchases/Views/EditProduct.xaml.cs
+++ MyPurchases/Views/EditProduct.xaml.cs
@@ -1,3 +1,5 @@
+using MyPurchases.Models;
+
 namespace MyPurchases.Views;
 
 public partial class EditProduct : ContentPage
@@ -6,4 +8,29 @@ public partial class EditProduct : ContentPage
 	{
 		InitializeComponent();
 	}
+
+    private async void ToolbarItem_Clicked(object sender, EventArgs e)
+    {
+        try
+        {
+            Product product_attached = BindingContext as Product;
+
+            Product product = new Product
+            {   
+                Id = product_attached.Id,
+                Description = txt_description.Text,
+                Amount = Convert.ToDouble(txt_amount.Text),
+                Price = Convert.ToDouble(txt_price.Text)
+            };
+
+            await App.Db.Update(product);
+            await DisplayAlert("Success!", "Product Updated", "Ok");
+            await Navigation.PopAsync();
+
+        }
+        catch (Exception ex)
+        {
+            await DisplayAlert("Ops", ex.Message, "Ok");
+        }
+    }
 }
