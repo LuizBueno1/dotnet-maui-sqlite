a62c07a5f75ea09717e1964ec4c9a1edba439174
Luiz Henrique Bueno <luizbueno.dev@gmail.com>
2025-09-03 22:35:23 -0300

    Add validations

--- MyPurchases/Models/Product.cs
+++ MyPurchases/Models/Product.cs
@@ -4,9 +4,23 @@ namespace MyPurchases.Models
 {
     public class Product
     {
+        string _description;
+
         [PrimaryKey, AutoIncrement]
         public int Id { get; set; }
-        public string Description { get; set; }
+        public string Description 
+        {
+            get => _description;
+            set
+            {
+                if (value == null) 
+                {
+                    throw new Exception("Please, fill in the description.");
+                }
+
+                _description = value;
+            }
+        }
         public double Amount { get; set; }
         public double Price { get; set; }
         public double Total { get => Amount * Price;}
